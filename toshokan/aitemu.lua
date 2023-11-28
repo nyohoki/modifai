613554bce433fdc29989923df158c84abf66667f
nyohoki <nyohoki@gmail.com>
2023-11-28 14:07:45 -0800

    Update aitemu.lua

--- toshokan/aitemu.lua
+++ toshokan/aitemu.lua
@@ -1,12 +1,110 @@
-data:extend({
+data:extend(
 	{
-		type = "item",
-		name = "heavy_metal_plate",
-		icon = "__modifai__/gurafikku/aikon/heavy_metal_plate.png",
-		icon_size = 32,
-		icon_scale = 1,
-		subgroup = "nyohoki-intermediates",
-		order = "hmp",
-		stack_size = 100
+		{
+			type = "item",
+			name = "heavy_metal_plate",
+			icons = {
+				{
+					icon = "__modifai__/images/icons/heavy_metal_plate.png",
+					icon_size = 32,
+					icon_scale = 1
+				}
+			},
+			subgroup = nyomeds,
+			order = "hmp",
+			stack_size = 100
+		},
+		{
+			type = "item",
+			name = "ore-copper",
+			icons = {
+				{
+					icon = "__modifai__/images/icons/ore-copper.png",
+					icon_size = 32,
+					icon_scale = 1
+				}
+			},
+			subgroup = nyomeds,
+			order = "ore-copper",
+			stack_size = orestack
+		},
+		{
+			type = "item",
+			name = "ore-darksteel",
+			icons = nyohoki_icon,
+			subgroup = nyomeds,
+			order = "ore-darksteel",
+			stack_size = orestack
+		},
+		{
+			type = "item",
+			name = "ore-gold",
+			icons = nyohoki_icon,
+			subgroup = nyomeds,
+			order = "ore-gold",
+			stack_size = orestack
+		},
+		{
+			type = "item",
+			name = "ore-iron",
+			icons = {
+				{
+					icon = "__modifai__/images/icons/ore-iron.png",
+					icon_size = 32,
+					icon_scale = 1
+				}
+			},
+			subgroup = nyomeds,
+			order = "ore-iron",
+			stack_size = orestack
+		},
+		{
+			type = "item",
+			name = "ore-koppranickel",
+			icons = nyohoki_icon,
+			subgroup = nyomeds,
+			order = "ore-koppranickel",
+			stack_size = orestack
+		},
+		{
+			type = "item",
+			name = "ore-lead",
+			icons = nyohoki_icon,
+			subgroup = nyomeds,
+			order = "ore-lead",
+			stack_size = orestack
+		},
+		{
+			type = "item",
+			name = "ore-mythril",
+			icons = nyohoki_icon,
+			subgroup = nyomeds,
+			order = "ore-mythril",
+			stack_size = orestack
+		},
+		{
+			type = "item",
+			name = "ore-silver",
+			icons = nyohoki_icon,
+			subgroup = nyomeds,
+			order = "ore-silver",
+			stack_size = orestack
+		},
+		{
+			type = "item",
+			name = "ore-tin",
+			icons = nyohoki_icon,
+			subgroup = nyomeds,
+			order = "ore-tin",
+			stack_size = orestack
+		},
+		{
+			type = "item",
+			name = "ore-zinc",
+			icons = nyohoki_icon,
+			subgroup = nyomeds,
+			order = "ore-zinc",
+			stack_size = orestack
+		}
 	}
-})
+)
